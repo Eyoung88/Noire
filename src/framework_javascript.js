e3ca31c47f4a38b627c7c7afdaf18983c09ea301
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-05-11 08:55:39 -0600

    Created a framework file to test the toppings list

---
+++ src/framework_javascript.js
@@ -0,0 +1,37 @@
+const toppings = document.getElementsByClassName("toppings");
+
+Array.from(toppings).forEach(function (element) {
+    element.innerHTML += `<div class="toppings_box">
+    <form class="pizza_topping_form">
+        <div class="topping_amount">
+            <label class="container">Extra
+                <input type="radio" name="topping_amount" value="Extra">
+                <span class="checkmark"></span>
+            </label>
+            <label class="container">Normal
+                <input type="radio" name="topping_amount" value="Normal">
+                <span class="checkmark"></span>
+            </label>
+            <label class="container">None
+                <input type="radio" name="topping_amount" value="None">
+                <span class="checkmark"></span>
+            </label>
+        </div>
+        <div class="portion_of_pizza">
+            <label class="container">Left Half
+                <input type="radio" name="pizza_portion" value="left">
+                <span class="checkmark"></span>
+            </label>
+            <label class="container">Normal
+                <input type="radio" name="pizza_portion" value="right">
+                <span class="checkmark"></span>
+            </label>
+            <label class="container">None
+                <input type="radio" name="pizza_portion" value="full">
+                <span class="checkmark"></span>
+            </label>
+            <input type="submit" value="Submit">
+        </div>
+    </form>
+</div>`;
+});
