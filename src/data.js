d3df3b5e07942c3ad06067d841ed7886fb3f90eb
Eric Young <eyoung88@frontier.com>
2020-05-15 11:17:00 -0600

    Add files for create and price start but has issues

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export let data = {
+    prices: [
+        {"x-large": "$20.00"},
+        {'large': "$15.00"},
+        {'medium': '$12.00'},
+        {'small': '$8.00'}
+    ]
+    //Need to add toppings here
+}
--- src/prices.js
+++ src/prices.js
@@ -3,10 +3,28 @@ toggle_visibility('special-deal');
 
 function toggle_visibility(id) {
    var e = document.getElementById(id);
-   if(e.style.display == 'block')
+   if (e.style.display == 'block')
       e.style.display = 'none';
    else
       e.style.display = 'block';
 }
 
+var radioChecked = document.getElementById('inputBtn').addEventListener('click', isChecked);
+
+export const isChecked = (evt)  => {
+   let price = 0;
+   
+   if(document.getElementById('inputBtn').click()) {
+      document.getElementById("order").innerHTML = "Your current price is: " + price;
+   }
+   else {
+      document.getElementById('order').innerHTML = "Please select an option";
+      evt.preventDefault();
+   }
+
+}
+
+
+
 //Add functionality to count selected toppings until 5
+// e.preventDefault function on form
