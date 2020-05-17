f6722410de0db9f00a149346c14ef30dda1681f1
Eric Young <eyoung88@frontier.com>
2020-05-17 17:50:44 -0600

    Prebuild add

--- src/data.js
+++ src/data.js
@@ -92,5 +92,78 @@ export let data = {
             "name": "anchovy",
             "file_prefix": "anchovy"
         }
+    ],
+    prebuilt3: [
+        { 'medium': '$12.00' },
+        {
+            "name": "crust",
+            "file_prefix": "pizza_canvas"
+        },
+        {
+            "name": "bell_peppers",
+            "file_prefix": "bell_peppers"
+        },
+        {
+            "name": "jalepenos",
+            "file_prefix": "jalepenos"
+        },
+        {
+            "name": "olives",
+            "file_prefix": "olives"
+        },
+        {
+            "name": "pineapple",
+            "file_prefix": "pineapple"
+        }
+    ],
+    prebuilt4: [
+        { "x-large": "$20.00" },
+        {
+            "name": "crust",
+            "file_prefix": "pizza_canvas"
+        },
+        {
+            "name": "cheese",
+            "file_prefix": "cheese"
+        },
+        {
+            "name": "ham",
+            "file_prefix": "ham"
+        },
+        {
+            "name": "anchovy",
+            "file_prefix": "anchovy"
+        },
+        {
+            "name": "bacon",
+            "file_prefix": "bacon"
+        },
+        {
+            "name": "sausage",
+            "file_prefix": "sausage"
+        },
+        {
+            "name": "pepperoni",
+            "file_prefix": "pepperoni"
+        }
+    ],
+    prebuilt5: [
+        { 'medium': '$12.00' },
+        {
+            "name": "crust",
+            "file_prefix": "pizza_canvas"
+        },
+        {
+            "name": "cheese",
+            "file_prefix": "cheese"
+        },
+        {
+            "name": "ham",
+            "file_prefix": "ham"
+        },
+        {
+            "name": "pineapple",
+            "file_prefix": "pineapple"
+        }
     ]
 }
