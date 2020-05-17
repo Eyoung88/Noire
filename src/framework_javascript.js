864a0e4b2ad5fdb85292f77fdf12d9dc775217cc
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-05-17 16:37:20 -0600

    You can now select for each topping

--- src/framework_javascript.js
+++ src/framework_javascript.js
@@ -2,36 +2,35 @@ const toppings = document.getElementsByClassName("toppings");
 
 Array.from(toppings).forEach(function (element) {
     element.innerHTML += `<div class="toppings_box">
-    <input class="test" type="checkbox">
         <div class="topping_amount">
             <label class="container">
-                <input class="img_radio" type="radio" name="topping_amount" value="Extra">
+                <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="Extra">
                 <img class="topping_resize" src="../images/Extra.png">
             </label>
             <label class="container">
-                <input class="img_radio" type="radio" name="topping_amount" value="Normal" checked>
+                <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="Normal" checked>
                 <img class="topping_resize" src="../images/Normal.png">
             </label>
             <label class="container">
-                <input class="img_radio" type="radio" name="topping_amount" value="None">
+                <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="None">
                 <img class="topping_resize" src="../images/None.png">
             </label>
         </div>
         <div class="portion_of_pizza">
             <label>
-                <input class="img_radio" type="radio" name="test" value="left">
+                <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="left">
                 <img class="img_resize" src="../images/PizzaPortionLeft.png">
             </label>
 
             <label>
-                <input class="img_radio" type="radio" name="test" value="full" checked>
+                <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="full" checked>
                 <img class="img_resize" src="../images/PizzaPortionFull.png">
             </label>
 
             <label>
-                <input class="img_radio" type="radio" name="test" value="right">
+                <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="right">
                 <img class="img_resize" src="../images/PizzaPortionRight.png">
             </label>
         </div>
-
-</div>`;});
+</div>`;
+});
