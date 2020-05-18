6b31ce93ed27e0024572d4825551ebdc0b6267ed
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2020-05-18 16:06:07 -0600

    Moved foreach loop from framework_js to index

--- src/index.js
+++ src/index.js
@@ -29,6 +29,43 @@ const topping_loader = () => {
 };
 topping_loader();
 
+Array.from(toppingArray).forEach(function (element) {
+    element.innerHTML += `<div class="toppings_box">
+        <div class="topping_amount">
+            <label class="container">
+                <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="Extra">
+                <img class="topping_resize" src="./images/Extra.png">
+            </label>
+            <label class="container">
+                <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="Normal" checked>
+                <img class="topping_resize" src="./images/Normal.png">
+            </label>
+            <label class="container">
+                <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="None">
+                <img class="topping_resize" src="./images/None.png">
+
+            </label>
+        </div>
+        <div class="portion_of_pizza">
+            <label>
+                <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="left">
+                <img class="img_resize" src="./images/PizzaPortionLeft.png">
+            </label>
+
+            <label>
+                <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="full" checked>
+                <img class="img_resize" src="./images/PizzaPortionFull.png">
+            </label>
+
+            <label>
+                <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="right">
+                <img class="img_resize" src="./images/PizzaPortionRight.png">
+            </label>
+        </div>
+
+</div>`;
+});
+
 
 const canvas = document.getElementById('pizza_view');
 
