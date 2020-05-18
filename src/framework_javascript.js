aaececebf6a0ae2fc5eb9c7a843f2bb0e43a3e2f
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-05-18 09:45:41 -0600

    images now load

--- src/framework_javascript.js
+++ src/framework_javascript.js
@@ -22,32 +22,32 @@ Array.from(toppings).forEach(function (element) {
         <div class="topping_amount">
             <label class="container">
                 <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="Extra">
-                <img class="topping_resize" src="../images/Extra.png">
+                <img class="topping_resize" src="./images/Extra.png">
             </label>
             <label class="container">
                 <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="Normal" checked>
-                <img class="topping_resize" src="../images/Normal.png">
+                <img class="topping_resize" src="./images/Normal.png">
             </label>
             <label class="container">
                 <input class="img_radio" type="radio" name="topping_amount_${element.id}" value="None">
-                <img class="topping_resize" src="../images/None.png">
+                <img class="topping_resize" src="./images/None.png">
 
             </label>
         </div>
         <div class="portion_of_pizza">
             <label>
                 <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="left">
-                <img class="img_resize" src="../images/PizzaPortionLeft.png">
+                <img class="img_resize" src="./images/PizzaPortionLeft.png">
             </label>
 
             <label>
                 <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="full" checked>
-                <img class="img_resize" src="../images/PizzaPortionFull.png">
+                <img class="img_resize" src="./images/PizzaPortionFull.png">
             </label>
 
             <label>
                 <input class="img_radio" type="radio" name="pizza_portion_${element.id}" value="right">
-                <img class="img_resize" src="../images/PizzaPortionRight.png">
+                <img class="img_resize" src="./images/PizzaPortionRight.png">
             </label>
         </div>
 </div>`;
